c9bb4e2439e3bb03b24ddc19f36547d057572cff
Daniil Rassadin <daniil@rassadin.org>
2023-12-20 14:22:39 +0600

    Initial Commit

---
+++ QuadraticEquation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QuadraticEquation
+//
+//  Created by Daniil Rassadin on 20/12/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
